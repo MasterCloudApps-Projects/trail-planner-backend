1011bb4838256f6915fa6a2306e3cd8ebc6f3bf0
ivanfermena <ivanfermena@gmail.com>
2022-11-03 20:14:32 +0100

    Add app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { FeatureFlagsModule } from './featureFlags/featureFlags.module';
 import { TrackModule } from './track/track.module';
+import { AppController } from './app/app.controller';
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { TrackModule } from './track/track.module';
     FeatureFlagsModule,
     TrackModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
