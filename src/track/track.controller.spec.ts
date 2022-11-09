e0d6c23b303bfed8e914ea43a69336913e2323a3
Pedro Carrasco <pedrocdeveloper@gmail.com>
2022-11-09 13:43:18 +0100

    Solve test dependency error

--- src/track/track.controller.spec.ts
+++ src/track/track.controller.spec.ts
@@ -2,18 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
 import fs from 'fs';
+import { TrackPointService } from '../trackPoint/trackPoint.service';
 
 describe('TrackController', () => {
   let trackController: TrackController;
   const mockTrackService = {};
+  const mockTrackPointService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TrackController],
-      providers: [TrackService],
+      providers: [TrackService, TrackPointService],
     })
       .overrideProvider(TrackService)
       .useValue(mockTrackService)
+      .overrideProvider(TrackPointService)
+      .useValue(mockTrackPointService)
       .compile();
 
     trackController = module.get<TrackController>(TrackController);
